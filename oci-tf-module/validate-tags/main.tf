2db68c43159588676932cefcf4de61fa79a2aea5
Dolis Sharma <71091713+dolisss@users.noreply.github.com>
2024-07-09 17:44:21 -0300

    Update main.tf

--- oci-tf-module/validate-tags/main.tf
+++ oci-tf-module/validate-tags/main.tf
@@ -1,1 +1,9 @@
+resource "oci_identity_compartment" "test_compartment" {
+    #Required
+    compartment_id = var.compartment_id
+    description = var.compartment_description
+    name = var.compartment_name
 
+    #Optional
+    defined_tags = {"Operations.CostCenter"= "42"}  //Policy to check the right tags
+    freeform_tags = {"Department"= "Finance"}  //Policy to check the right tags}
