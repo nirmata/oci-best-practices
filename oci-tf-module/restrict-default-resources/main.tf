001e563f5288efe6adc0dcf1656f774c0cf2e614
Dolis Sharma <71091713+dolisss@users.noreply.github.com>
2024-07-09 17:52:52 -0300

    Update main.tf

--- oci-tf-module/restrict-default-resources/main.tf
+++ oci-tf-module/restrict-default-resources/main.tf
@@ -1,6 +1,6 @@
 resource "oci_core_network_security_group" "example"  //Policy to ensure the 'default' pattern is not used in the resource type
 {
- compartment_id = var.compartment_id
- vcn_id         = oci_core_vcn.vcn1.id
+ compartment_id = 236489324032
+ vcn_id         = 3294032
  display_name   = "Example Security Group"
 }
