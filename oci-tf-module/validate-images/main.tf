2819cf0a41b4a9f133c87acbcd1482fded92ee6e
Dolis Sharma <71091713+dolisss@users.noreply.github.com>
2024-07-09 17:46:05 -0300

    Update main.tf

--- oci-tf-module/validate-images/main.tf
+++ oci-tf-module/validate-images/main.tf
@@ -1,1 +1,11 @@
+resource "oci_core_instance" "ic_pub_vm-A" {
+  compartment_id      = oci_identity_compartment.example_compartment.id
+  shape               = var.ic_pub_vm_A.shape.name
+  availability_domain = "xyz" // Add a Policy to validate
+  display_name        = var.ic_pub_vm_A.display_name
 
+  source_details {
+
+    source_type = "image"
+image_id = oci_core_image.test_image.id   // Policy to validate
+  }
