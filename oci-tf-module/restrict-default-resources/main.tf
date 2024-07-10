977878d7da61119d613734caf5c63d1d884ed70f
Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>
2024-07-10 14:07:37 -0400

    update TF and add policies
    Signed-off-by: Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>

--- oci-tf-module/restrict-default-resources/main.tf
+++ oci-tf-module/restrict-default-resources/main.tf
@@ -1,6 +1,56 @@
-resource "oci_core_network_security_group" "example"  //Policy to ensure the 'default' pattern is not used in the resource type
-{
- compartment_id = 236489324032
- vcn_id         = 3294032
- display_name   = "Example Security Group"
+resource "oci_core_vcn" "vcn1" {
+  cidr_block = "10.0.0.0/16"
+  dns_label = "vcn1"
+  compartment_id = var.compartment_id
+  display_name = "vcn1"
+}
+
+# Gateway
+## Internet Gateway
+resource "oci_core_internet_gateway" "inetGw" {
+    compartment_id = var.compartment_id
+    display_name   = "inetGw"
+    vcn_id         = oci_core_vcn.vcn1.id
+}
+
+resource "oci_core_default_dhcp_options" "default-dhcp-options" {
+  manage_default_resource_id = oci_core_vcn.vcn1.default_dhcp_options_id
+
+  #Required
+  options {
+    type = "DomainNameServer"
+    server_type = "VcnLocalPlusInternet"
+  }
+}
+
+  #Optional
+  options {
+    type = "SearchDomain"
+    search_domain_names = [ "abc.com" ]
+  }
+}
+
+resource "oci_core_default_route_table" "default-route-table" {
+  manage_default_resource_id = oci_core_vcn.vcn1.default_route_table_id
+
+    #Required
+    compartment_id = var.compartment_id
+
+    #Optional
+    display_name = "default_route_table"
+    route_rules {
+
+        #Required
+        network_entity_id = oci_core_internet_gateway.inetGw.id
+
+        #Optional
+        destination = "0.0.0.0/0"
+        destination_type = "CIDR_BLOCK"
+    }
+}
+
+resource "oci_core_network_security_group" "example" {
+  compartment_id = 236489324032
+  vcn_id         = vcn1
+  display_name   = "Example Security Group"
 }
