d814957fff20945c0452a051ba3e93112a7e764f
Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>
2024-07-10 17:55:24 -0400

    fix the tag policy
    Signed-off-by: Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>

--- oci-tf-module/validate-tags/main.tf
+++ oci-tf-module/validate-tags/main.tf
@@ -8,3 +8,14 @@ resource "oci_identity_compartment" "test_compartment" {
     defined_tags = {"Operations.CostCenter"= "42"}  //Policy to check the right tags
     freeform_tags = {"Department"= "Finance"}  //Policy to check the right tags}
 }
+
+resource "oci_identity_compartment" "test_compartment2" {
+    #Required
+    compartment_id = 13137101
+    description = "this is a new compartment"
+    name = oci-scan
+
+    #Optional
+    defined_tags = {"Operations.CostCenter"= "42"}  //Policy to check the right tags
+    freeform_tags = {"Department"= "Something Else"}  //Policy to check the right tags}
+}
