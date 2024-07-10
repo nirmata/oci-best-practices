acb6fcb28f838879167f84b934e975b9ff1d8524
Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>
2024-07-10 18:38:01 -0400

    add a policy to verify domain name usage in images and availability zones
    Signed-off-by: Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>

--- oci-tf-module/validate-images/main.tf
+++ oci-tf-module/validate-images/main.tf
@@ -1,11 +1,23 @@
 resource "oci_core_instance" "ic_pub_vm-A" {
   compartment_id      = 12439
   shape               = dev-nirmata
-  availability_domain = "kIdk:PHX-AD-1"
+  availability_domain = "oci.domain.name.tiaa.com"
   display_name        = oci-scan
 
   source_details {
     source_type = "image"
     image_id = oci_core_image.test_image.id   // Policy to validate
+  }
+}
+
+resource "oci_core_instance" "ic_pub_vm-B" {
+  compartment_id      = 12439
+  shape               = dev-nirmata
+  availability_domain = "oci.domain.name.tiaa.com"
+  display_name        = oci-scan
+
+  source_details {
+    source_type = "image"
+    image_id = oci.domain.name.tiaa.com.test_image.id   // Policy to validate
   }
 }
