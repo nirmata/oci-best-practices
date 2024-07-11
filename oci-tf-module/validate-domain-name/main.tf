c5b7ee93ac4de1cfb57016e1647480774c374c05
Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>
2024-07-11 09:57:17 -0400

    push in scannable code
    Signed-off-by: Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>

--- oci-tf-module/validate-domain-name/main.tf
+++ oci-tf-module/validate-domain-name/main.tf
@@ -1,25 +1,23 @@
 resource "oci_core_instance" "ic_pub_vm-A" {
-  compartment_id      = oci_identity_compartment.example_compartment.id
-  shape               = var.ic_pub_vm_A.shape.name
-  availability_domain = "xyz"
-  display_name        = var.ic_pub_vm_A.display_name
+  compartment_id      = 12439
+  shape               = dev-nirmata
+  availability_domain = "oci.domain.name.tiaa.com"
+  display_name        = oci-scan
 
   source_details {
-
     source_type = "image"
-    image_id = oci_core_image.test_image.id
+    image_id = oci_core_image.test_image.id   // Policy to validate
   }
 }
 
 resource "oci_core_instance" "ic_pub_vm-B" {
-  compartment_id      = oci_identity_compartment.example_compartment.id
-  shape               = var.ic_pub_vm_A.shape.name
-  availability_domain = "xyz"
-  display_name        = var.ic_pub_vm_A.display_name
+  compartment_id      = 12439
+  shape               = dev-nirmata
+  availability_domain = "oci.domain.name.tiaa.com"
+  display_name        = oci-scan
 
   source_details {
-
     source_type = "image"
-    image_id = oci_core_image.test_image.id
+    image_id = oci.domain.name.tiaa.com.test_image.id   // Policy to validate
   }
 }
