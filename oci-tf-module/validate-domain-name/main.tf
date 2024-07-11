06f75f59e7004c0afc5768a0282538335b785fbe
Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>
2024-07-11 09:55:14 -0400

    update TF code so its correct
    Signed-off-by: Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>

--- oci-tf-module/validate-domain-name/main.tf
+++ oci-tf-module/validate-domain-name/main.tf
@@ -7,5 +7,19 @@ resource "oci_core_instance" "ic_pub_vm-A" {
   source_details {
 
     source_type = "image"
-image_id = oci_core_image.test_image.id 
+    image_id = oci_core_image.test_image.id
   }
+}
+
+resource "oci_core_instance" "ic_pub_vm-B" {
+  compartment_id      = oci_identity_compartment.example_compartment.id
+  shape               = var.ic_pub_vm_A.shape.name
+  availability_domain = "xyz"
+  display_name        = var.ic_pub_vm_A.display_name
+
+  source_details {
+
+    source_type = "image"
+    image_id = oci_core_image.test_image.id
+  }
+}
