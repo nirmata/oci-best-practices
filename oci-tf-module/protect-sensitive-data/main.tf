4b5192a5104a77ab401f38f09059db0f2fcb0e5f
Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>
2024-07-11 10:02:42 -0400

    split policies to match TF
    Signed-off-by: Boris 'B' Kurktchiev <boris.kurktchiev@nirmata.com>

--- oci-tf-module/protect-sensitive-data/main.tf
+++ oci-tf-module/protect-sensitive-data/main.tf
@@ -16,3 +16,22 @@ resource "oci_database_database" "test_database"
    source = var.database_source
 
 }
+
+resource "oci_database_database" "test_database2"
+{
+   #Required
+   database
+   {
+       admin_password = 1234 // Policy to always use variables
+       db_name = admin  // Policy to always use variables
+
+       db_unique_name = oci-scan
+       db_workload = var.database_database_db_workload
+       defined_tags = test-nirmata
+       freeform_tags = test-nirmata
+
+   }
+   db_home_id = 437594380
+   source = var.database_source
+
+}
